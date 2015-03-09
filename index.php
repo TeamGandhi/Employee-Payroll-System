d5921fb0ab83cc451f45377832361167999c62dc
anudeepkota <anudeepkota7@gmail.com>
2015-03-09 08:31:25 -0400

    Created login page

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Team Gandhi</title>
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+    </head>
+   <body>
+      <div class="container">
+         <header id="header" class="page-header col-lg-12">
+            <h1> <small>Employee Payroll System</small></h1>
+         </header>
+      </div>
+      <div class="container">
+         <section id="login" class="col-lg-offset-4 col-lg-4">
+            <h2>Login</h2>
+            <form class="form">
+               <div class="form-group">
+                  <input type="text" class="form-control" id="input_hruname" placeholder="username" />
+               </div>
+               <div class="form-group">
+                  <input type="password" class="form-control" id="input_password" placeholder="Password">
+               </div>
+               <div class="form-group">
+                  <div class="col-sm-offset-2 col-sm-10">
+                     <button type="button" id="button_login" class="btn btn-default">Sign in</button>
+                  </div>
+               </div>
+            </form>
+         </section>
+      </div>
+      <div class="container">
+         <footer id="footer" class="footer col-lg-12">
+            <ul class="list-inline">
+                  <li><h4>Copyright: Team Gandhi</h4></li>
+            </ul>
+         </footer>
+      </div>
+   </body>
+</html>
