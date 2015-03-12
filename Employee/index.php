ff85cc2462814d53ab134ed2316b53dbd2cffa25
chand05 <chand11_11@ymail.com>
2015-03-12 14:20:13 -0400

    Create index.php

---
+++ Employee/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Team Gandhi </title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    </head>
+    <body>
+       
+    </body>
+</html>
