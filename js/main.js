dcaa6126477bc3d14f1277ada13cfffe1bfd2da9
Davinder Sharma <davinderpreet3@gmail.com>
2015-03-12 01:22:14 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,99 @@
-function setPage(page) {
-    $(".page").hide();
-    $("#" + page).show();
-}
+$(document).ready(function () {
 
-function addEmployee(id, username, firstName, lastName, salary, phone) {
-    var userStr = "(" + id + ") " + firstName+" " + lastName + " (" + username + ", " + salary + "$)";
+    $("#menuAddEmployee").click(function () {
+        setPage("pageAddEmployee");
+    });
 
-    $("#employeeList").append("<option value='" + id + "'>" + userStr +"</option>");
-}
+    $("#menuViewEmployees").click(function () {
+        setPage("pageViewEmployees");
+    });
 
-function removeEmployee(id) {
-    $("#employeeList option[value=" + id + "]").remove();
-}
-
-function refreshEmployees() {
-    $("#employeeList").children().remove();
-    $.post("php/employees.php", {list: true}, function (data) {
-        var employees = JSON.parse(data);
-        employees.forEach(function (element) {
-            addEmployee(element.Id, element.Username, element.FirstName, element.LastName, element.Salary, element.Phone);
+    $("#vieweditEmployeeButton").click(function () {
+        var id = $("#employeeList").val();
+        $.post("php/employees.php", {get: true, id: id}, function (data) {
+            var employee = JSON.parse(data);
+            $("#editFirstName").val(employee.FirstName);
+            $("#editLastName").val(employee.LastName);
+            $("#editSalary").val(employee.Salary);
+            $("#editUsername").val(employee.Username);
+            $("#editPhone").val(employee.Phone);
+            $("#editPassword").val(employee.Password);
+            $("#editDesignation").val(employee.Designation);
+            $("#editAddress").val(employee.Address);
+            setPage("pageEditEmployee");
         });
     });
-}
+
+    $("#menuLogout").click(function () {
+        $.ajax("php/logout.php");
+        document.location.href = "";
+    });
+
+    $("#addEmployeeButton").click(function (event) {
+        event.preventDefault();
+
+        var firstName = $("#addFirstName").val();
+        var lastName = $("#addLastName").val();
+        var salary = $("#addSalary").val();
+        var username = $("#addUsername").val();
+        var phone = $("#addPhone").val();
+        var password = $("#addPassword").val();
+        var designation = $("#addDesignation").val();
+        var address = $("#addAddress").val();
+
+        $.post("php/employees.php", {
+            add: true,
+            username:username,
+            firstName: firstName,
+            lastName: lastName,
+            salary: salary,
+            phone:phone,
+            password:password,
+            designation:designation,
+            address:address
+        }, function (data) {
+            refreshEmployees();
+            setPage("pageViewEmployees");
+        });
+    });
+
+    $("#editEmployeeButton").click(function (event) {
+        event.preventDefault();
+
+        var firstName = $("#editFirstName").val();
+        var lastName = $("#editLastName").val();
+        var salary = $("#editSalary").val();
+        var id = $("#employeeList").val();
+        var username = $("#editUsername").val();
+        var phone = $("#editPhone").val();
+        var password = $("#editPassword").val();
+        var designation = $("#editDesignation").val();
+        var address = $("#editAddress").val();
+
+        $.post("php/employees.php", {
+            update: true,
+            id: id,
+            username:username,
+            firstName: firstName,
+            lastName: lastName,
+            salary: salary,
+            phone:phone,
+            password:password,
+            designation:designation,
+            address:address
+        }, function (data) {
+            refreshEmployees();
+            setPage("pageViewEmployees");
+        });
+    });
+
+    $("#removeEmployeeButton").click(function (event) {
+        event.preventDefault();
+        var id = $("#employeeList").val();
+        $.post("php/employees.php", {remove: true, id: id});
+        removeEmployee(id);
+    });
+
+    refreshEmployees();
+    setPage("pageAddEmployee");
+});
