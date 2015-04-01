f7f9523574c861292b1930a7382e51eab5723c3b
jasleen-kaur17 <jasleenhundal17@gmail.com>
2015-04-01 15:13:27 -0400

    Create employee_leave.php

---
+++ employee_leave.php
@@ -0,0 +1,27 @@
+<html>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap.css">
+    <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/dataTables.bootstrap.js"></script>
+    <body>
+        <h2>Leaves</h2>
+        <table border="0" class="table table-striped table-bordered" id="leaves_list" aria-describedby="example_info">
+            <thead>
+                <tr>
+                    
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>From Date</th>
+                    <th>To Date</th>
+                    <th>Status</th>
+                   
+                </tr>
+            </thead>
+            <tbody>
+                
+                
+            </tbody>
+        </table>
+    </body>
