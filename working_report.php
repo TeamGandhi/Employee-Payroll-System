6f4d7f6bed2c6741c629fd02933e476e97e40859
jasleen-kaur17 <jasleenhundal17@gmail.com>
2015-04-01 15:09:54 -0400

    Create working_report.php

---
+++ working_report.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap.css">
+<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/dataTables.bootstrap.js"></script>
+<body>
+    <h1>Employee Payrolls List</h1>
+    <br>
+    <div class="container">
+        <!-- To Get Months List -->
+        <select id="month">
+           
+        </select>
+        <!-- To Get Years List -->
+        <select id="year">
+                   </select>
+         }
+        ?>
+    </div>
+    <div class="table-responsive">
+        <table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered dataTable" id="working_report" aria-describedby="example_info">
+            <thead>
+                <tr>
+                    <th>Employee Name</th>
+                    <th>Designation</th>
+                    <th>Actual Monthly Salary</th>
+                    <th>Total Working Hours</th>
+                    <th>Month Salary</th>
+                </tr>
+            </thead>
+            <tbody>
+              
+            </tbody>
+        </table>
+    </div>
