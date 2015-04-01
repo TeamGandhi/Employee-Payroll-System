39b8cb9c2a2bfb6e6d082c0250f22b154be6d83b
anudeepkota <anudeepkota7@gmail.com>
2015-04-01 13:36:40 -0400

    Update times.php

--- Employee/php/times.php
+++ Employee/php/times.php
@@ -2,22 +2,28 @@
 
 include_once "dbConnection.php";
  session_start();
-class Times
-{
-
-    static function insertTime($date, $time)
+    class Times
     {
-        $employee = Db::query("SELECT * FROM Employees WHERE Username = ?", $_SESSION["username"]);	
-        return Db::query("INSERT INTO times (date,time,employee_id) VALUES (?,?,?)", array($date, $time, $employee[0]['Id']));
+
+        static function insertTime($date, $timeIn,$timeOut,$working_hours)
+        {
+            $employee = Db::query("SELECT * FROM Employees WHERE Username = ?", $_SESSION["username"]);
+            Db::query("INSERT INTO times (date,time, timeout,hours,employee_id) VALUES (?,?,?,?,?)", array($date, $timeIn, $timeOut, $working_hours,$employee[0]['Id']));
+            return $employee[0]['Id'];
+        }
+
+    }
+    //Find total num of hours worked
+    function timeDifference($timeEnd, $timeStart){
+      $tResult = strtotime($timeEnd) - strtotime($timeStart);
+      return date("G:i", $tResult);
+    }
+    
+    if (isset($_POST["insert"])) {
+        $working_hours = timeDifference($_POST['timeOut'], $_POST["timeIn"]);
+        Times::insertTime($_POST["date"],$_POST["timeIn"],$_POST['timeOut'],$working_hours);
     }
 
-}
-
- if (isset($_POST["insert"]))
-{
-
-    Times::insertTime($_POST["date"],$_POST["time"]);
-}
 
 
 
