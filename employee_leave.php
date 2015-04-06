dabcd25e333e6e4c5b78b451180b2d6cbd158839
Davinder Sharma <davinderpreet3@gmail.com>
2015-04-06 10:03:24 -0400

    Update employee_leave.php

--- employee_leave.php
+++ employee_leave.php
@@ -2,40 +2,39 @@
 include "php/leaves_list.php";
 ?>
 <html>
-    <head>
-        <title>Team Gandhi</title>
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap.css">
-        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/dataTables.bootstrap.js"></script>
-    </head>
-    
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/dataTables.bootstrap.css">
+    <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/dataTables.bootstrap.js"></script>
     <body>
-        <?php if ($msg !="") { ?>
-                    <div class="alert alert-success">
-                        <a href="#" class="close" data-dismiss="alert">&times;</a>
-                        <?php echo $msg; ?>
-                    </div>
-        <?php } ?>
-        
         <h2>Leaves</h2>
         <table border="0" class="table table-striped table-bordered" id="leaves_list" aria-describedby="example_info">
             <thead>
                 <tr>
+                    <?php if ($url_info[2] !='employee') { ?>
+                        <th>User Name</th>
+                    <?php } ?>
                     <th>Name</th>
                     <th>Description</th>
                     <th>From Date</th>
                     <th>To Date</th>
                     <th>Status</th>
-                    <th>Action</th>
+                    <?php if ($url_info[2] !='employee') { ?>
+                            <th>Action</th>
+                    <?php } ?>
                 </tr>
             </thead>
             <tbody>
                 <?php
                 foreach ($leaves_list as $leave):  ?>
                     <tr>
+                        <?php if ($url_info[2] !='employee') { ?>
+                            <td>
+                                <?php echo $leave['Username']; ?>
+                            </td>
+                        <?php } ?>
                         <td>
                             <?php echo $leave['name']; ?>
                         </td>
@@ -59,9 +58,11 @@ include "php/leaves_list.php";
                                 }
                             ?>
                         </td>
-                        <td>
-                            <button class="btn btn-primary delete_leave" onclick='confirm_delete("<?php echo $leave['id']; ?>","<?php echo $leave['name']; ?>"); return false;'>Delete</button>
-                        </td>
+                        <?php if ($url_info[2] !='employee') { ?>
+                                <td>
+                                    <a href="edit_leave.php?id=<?php echo $leave['id']; ?>">Edit</a>
+                                </td>
+                        <?php } ?>
                     </tr>
                 <?php
                 endforeach;
@@ -69,5 +70,7 @@ include "php/leaves_list.php";
             </tbody>
         </table>
     </body>
-    
+    <script>
+        $('#leaves_list').dataTable();
+    </script>
 </html>
